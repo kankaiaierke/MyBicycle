67be371b77122ea11b2f5c4e4de5ef515dcd9d2a
阚凯 <kankai@kankaideMacBook-Air.local>
2019-07-19 11:55:54 +0800

    环境部署

--- mybicycle/src/pages/demo/Life.js
+++ mybicycle/src/pages/demo/Life.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Child from './Child'
 import './index.less'
+import {Button} from 'antd'
 
 export default class Life extends React.Component{
-    constructor(props){
+    /*constructor(props){
         super(props);
         this.state = {
             count:0,
         }
+    }*/
+
+    state = {
+        count:0,
     }
 
     handleAdd=()=>{
@@ -28,7 +33,7 @@ export default class Life extends React.Component{
         }
         return <div className="content">
             <p>react生命周期介绍</p>
-            <button onClick={this.handleAdd}>点击一下</button>
+            <Button type="primary" onClick={this.handleAdd}>点击一下</Button>
             <button onClick={this.handleClick.bind(this)}>点击一下</button>
             <p>{this.state.count}</p>
             <Child name={this.state.count}></Child>
